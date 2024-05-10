580e64bcc53c26822f538f61aace1db8a182a0f9
exelarios <derickwok25@gmail.com>
2024-05-10 00:30:37 -0700

    feat: add underline for links tags on hover

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,20 +3,20 @@ function Footer() {
     <footer className="my-10 p-3 absolute bottom-0">
       <ul className="flex gap-x-5">
         <li>
-          <a href="https://linkedin.com/in/derickwok">linkedin</a>
+          <a className="hover:underline" href="https://linkedin.com/in/derickwok">linkedin</a>
         </li>
         <li>
-          <a href="https://github.com/exelarios">
+          <a className="hover:underline" href="https://github.com/exelarios">
             github
           </a>
         </li>
         <li>
-          <a href="mailto:derickwok25@gmail.com">
+          <a className="hover:underline" href="mailto:derickwok25@gmail.com">
             email
           </a>
         </li>
         <li>
-          <a href="https://open.spotify.com/user/urg0ynqt900kvykk1or129aqc">
+          <a className="hover:underline" href="https://open.spotify.com/user/urg0ynqt900kvykk1or129aqc">
             spotify
           </a>
         </li>
