ea812af18dba3c0937b88240453c40ccefb12ef3
exelarios <derickwok25@gmail.com>
2024-07-20 03:26:51 -0700

    chore: work experience, color for anchor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="px-5">
       <h1 className="font-semibold text-2xl">Deric Kwok</h1>
-      <p className="my-2">I like to build things -- mostly software.</p>
+      <p className="mt-2">I like to build things -- mostly software.</p>
+      <p>
+        Building at <Link href="https://clerkie.io" className="underline">Clerkie</Link>,
+        previously at <Link href="https://coinbase.com" className="underline">Coinbase</Link> & <Link className="underline" href="https://garden.zendesk.com">Zendesk</Link>
+      </p>
       <details className="my-5">
         <summary>Things im into</summary>
         <ul>
